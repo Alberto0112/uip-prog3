78cb87bbffed35b42c9d2a8aac7a0371730a44d6
Alberto0112 <albertojuarez1991@outlook.com>
2016-02-01 18:56:31 -0500

    laboratorio

---
+++ python/lab1.py
@@ -0,0 +1,17 @@
+input("suma")
+x = 5000
+suma = 0
+
+
+while x >= 10:
+           if  x == 100 or x == 1000:
+                     pass
+                     if x%2==0:
+                      suma += x
+                      x -= 1
+
+
+print ("total es: %i" % suma)
+
+
+
